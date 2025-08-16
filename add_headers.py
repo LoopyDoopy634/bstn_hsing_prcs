667a1cdd3b945da632a054180a7ac900515c3026
Aditya Paul <adityapaul1112@gmail.com>
2025-08-16 18:19:48 +0530

    Added comments to add_headers.py for ease of understanding. Reminder to add comments and then push after each feature.

--- add_headers.py
+++ add_headers.py
@@ -4,23 +4,26 @@ def add_header_if_missing(filename, header):
     
     try:
         with open(filename, 'r') as f:
-            first_line = f.readline().strip()
+            first_line = f.readline().strip()   #Read the first line of the csv file
+            
+            #If the first line has a numerical value then converting to float is fine.
+            #If the first line has a text value then converting to float leads to error.
             
             try:
-                float(first_line.split()[0])
-                has_header = False
-            except (ValueError, IndexError):
+                float(first_line.split()[0])   #Take the first character from the current header and convert it into float.
+                has_header = False   #Set a boolean to check if headers are there or not.
+            except (ValueError, IndexError):   #Wrong conversion will give error.
                 has_header = True
         
         if not has_header:
             print(f"'{filename}' is  missing a header. Adding one now...")
             
             with open(filename, 'r') as f:
-                original_content = f.read()
+                original_content = f.read()   #Read the entire contents of the current csv file.
                 
             with open(filename, 'w') as f:
-                f.write(' '.join(header) + '\n')
-                f.write(original_content)
+                f.write(' '.join(header) + '\n')   #Add the headings first.
+                f.write(original_content)   #After the headings, add the rest of the dataset.
         
         else:
             print(f"'{filename}' already has a header. No changes were made.")
