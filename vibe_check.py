cf46504e748f88e48f14abd423c103a23f6b11b1
Aditya Paul <adityapaul1112@gmail.com>
2025-08-16 14:33:49 +0530

    Downloaded and went through the data along with running a few basic commands to check the data

---
+++ vibe_check.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.read_csv('dataset/housing.csv')
+
+print(df.head())   #head prints the first 5 columns from the dataset
+
+print(df.info())    #info gives a technical summary of the dataset i.e. the columns, number of non-null entries, etc
+
+print(df.describe())    #describe gives statistical summary like highest values, unique values, etc
+
+print(df.isnull().sum())    #tells us if there are any missing values to handle
