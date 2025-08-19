b47a683d81b7b38ea440f1c247e444ca636d99e1
Aditya Paul <adityapaul1112@gmail.com>
2025-08-19 11:32:05 +0530

    Added a script that gives a visualization of the multi-feature decision tree model.

---
+++ visualize_dec_tree_2.py
@@ -0,0 +1,28 @@
+#This script gives a visualization of our multi-feature decision tree model.
+
+import matplotlib.pyplot as plt   #Used for generating digarams.
+from sklearn.tree import plot_tree   #Used for complex diagrams.
+import joblib   #Used for saving and loading our models.
+import os   #Used to create folders and save the generated image in it.
+
+model_folder = 'models'
+model_filename = 'multi_feature_model.joblib'
+full_path = os.path.join(model_folder, model_filename)   #This is the path in which the model is saved.
+
+model = joblib.load(full_path)   #Load the model from the path.
+print(f"Model loaded from '{full_path}'")
+
+image_folder = 'images'
+os.makedirs(image_folder, exist_ok=True)   #Create an images folder if it doesn't exist.
+
+print("Generating new tree visualization...")
+plt.figure(figsize=(20, 10))   #Create a figure of width 20 and height 10.
+
+plot_tree(model, feature_names=['RM', 'LSTAT'], filled=True, rounded=True, fontsize=10)   #Use the model to plot it on the figure.
+plt.title("Multi-Feature Decision Tree (RM & LSTAT)", fontsize=16)
+output_image_file = 'multi_feature_tree.png'
+image_path = os.path.join(image_folder, output_image_file)   #Create the path for saving the image.
+plt.savefig(image_path)   #Save the image.
+plt.close()
+
+print(f"New tree visualization saved to '{image_path}'")
